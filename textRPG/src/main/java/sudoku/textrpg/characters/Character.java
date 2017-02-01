5496eb6e2a028b701ca3db05207af10357ec45c7
otsepp <o.tuomas.seppanen@gmail.com>
2017-02-01 08:40:37 +0200

    update

--- textRPG/src/main/java/sudoku/textrpg/characters/Character.java
+++ textRPG/src/main/java/sudoku/textrpg/characters/Character.java
@@ -1,6 +1,8 @@
 
 package sudoku.textrpg.characters;
 
-public class Character {
-    
+public abstract class Character {
+    private String name;
+    private double health;
+    private double baseDamage;
 }
